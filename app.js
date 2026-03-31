625292394bb3556b81522fad74613c913d80f5b9
Jhay-R <castroverdejw@gmail.com>
2026-03-31 23:19:33 +0800

    feat: add comments

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+// event listener that waits for HTML document to be completely loaded and parsed
 document.addEventListener('DOMContentLoaded', ()=>{
+
+    // after HTML document is loaded and parsed get the form from html
     const form = document.querySelector('#application-form')
+
+    // when submit button is clicked/pressed 
     form.addEventListener('submit',(event)=>{
         event.preventDefault()
+
+        //store data from the form to a FormData obj
         const formData = new FormData(form)
+
+        // this display the content of formData to the console
         console.log(formData)    
     })
 })
